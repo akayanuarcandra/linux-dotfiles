c71d6007979de0f5a6c7bd8d0f313ce3c8534eb2
akayanuarcandra <hazumiya.yc@gmail.com>
2025-11-06 19:11:40 +0800

    add more

--- hypr/scripts/my_hypr_startup.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Hyprland startup script running as $(whoami)..." >> /tmp/hypr_startup.log
-
-sudo adguardvpn-cli connect -l SG
-echo "Privileged command executed." >> /tmp/hypr_startup.log
-
